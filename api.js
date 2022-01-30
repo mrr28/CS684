b72e77094eac0b7be44662c22a91f6c214cc7f9c
HarshalBhole <hb32@njit.edu>
2022-01-30 17:33:01 -0500

    Little changes in API and index file

--- api.js
+++ api.js
@@ -1,86 +1,77 @@
-//html tag that holds the team name 
-let teamheader = document.getElementById("teamheader")
-
-//input box for entering different team
-let enter_team = document.getElementById('enter_team')
+//html tag that holds the team name
+let teamheader = document.getElementById("teamheader");
 
 //html tag that holds the token value
-let tokenvalue = document.getElementById('tokenvalue')
+let tokenvalue = document.getElementById("tokenvalue");
 
 //html button that allows  you to call api without reloading the page
-let apicall = document.getElementById('apicall')
+let apicall = document.getElementById("apicall");
 
 //html button for submitting custom team
-let changeteam = document.getElementById('changeteam')
+let changeteam = document.getElementById("changeteam");
 
-let defaultteam = document.getElementById('defaultteam')
-
+let defaultteam = document.getElementById("defaultteam");
 
 //the url is the standard format for the api endpoint that
-//is holding the team name and token values 
-let url = "https://cs684api.7g2rji63c728k.us-east-1.cs.amazonlightsail.com/hw3?team="
+//is holding the team name and token values
+let url =
+    "https://cs684api.7g2rji63c728k.us-east-1.cs.amazonlightsail.com/hw3?team=";
 
-//global default team_name
-let team_name = ""
-
-
-//function that makes an ajax call to a remote 
+//function that makes an ajax call to a remote
 //api and updates the teamname and token values
 //in the html document with the requested values
 let getTeamInformation = () => {
-
-    //on window load set team name to default. Changing teams  
+    //on window load set team name to default. Changing teams
     //updates value
-    if(changeteam.style.display == "none"){
-        team_name = enter_team.value;
-    }
-    else{
-        team_name = getTeam()
+    if (changeteam.style.display == "none") {
+        team_name = teamheader.value;
+    } else {
+        team_name = getTeam();
     }
 
-    console.log(team_name)
+    console.log(team_name);
     $.ajax({
-        url:  url + team_name,
-        type : 'GET',
+        url: url + team_name,
+        type: "GET",
         crossDomain: true,
-        dataType:'json',
-        success: function(data) {           
-            teamheader.innerHTML = data.team
-            tokenvalue.innerHTML = data.token
+        dataType: "json",
+        success: function (data) {
+            teamheader.value = data.team;
+            tokenvalue.value = data.token;
         },
-        error: function(error)
-        {
-            console.log(error)
-        }
-      });
-}
+        error: function (error) {
+            console.log(error);
+        },
+    });
+};
 
-let searchCustom = (event) => {
-    if(teamheader.style.display != "none"){
-        teamheader.style.display = "none";
-        enter_team.style.display = "block";
+//Get Default team name and on page load
+let getTeam = () => {
+    return "Team Z";
+};
+
+let searchCustom = event => {
+    if (changeteam.style.display != "none") {
+        console.log("HERE UP serchcustom");
+        teamheader.value = "";
+        tokenvalue.value = "";
         defaultteam.style.display = "inline-block";
         changeteam.style.display = "none";
-    }
-    else{
-        teamheader.style.display = "block";
-        enter_team.style.display = "none";
-        defaultteam.style.display = "none";
+    } else {
+        console.log("HERE down serchcustom");
         changeteam.style.display = "inline-block";
-        teamheader.innerHTML = getTeam();
-        team_name = getTeam();
+        defaultteam.style.display = "none";
+        teamheader.value = getTeam();
         tokenvalue.innerHTML = "";
+        getTeamInformation();
     }
-}
-
-
+};
 
 //call function on window load
 window.onload = () => {
-    getTeamInformation()
-} 
+    getTeamInformation();
+};
 
-changeteam.addEventListener('click', searchCustom);
-defaultteam.addEventListener('click', searchCustom);
-apicall.addEventListener('click', getTeamInformation);
-
+changeteam.addEventListener("click", searchCustom);
+defaultteam.addEventListener("click", searchCustom);
+apicall.addEventListener("click", getTeamInformation);
