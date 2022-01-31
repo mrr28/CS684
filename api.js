f71bc3d3c409b3ab967708e3c3f430d2009cee77
DragonFish55 <61723984+DragonFish55@users.noreply.github.com>
2022-01-30 21:22:25 -0500

    Updated html and js file and removed css

--- api.js
+++ api.js
@@ -7,9 +7,7 @@ let tokenvalue = document.getElementById("tokenvalue");
 //html button that allows  you to call api without reloading the page
 let apicall = document.getElementById("apicall");
 
-//html button for submitting custom team
-let changeteam = document.getElementById("changeteam");
-
+//button for setting the default team
 let defaultteam = document.getElementById("defaultteam");
 
 //the url is the standard format for the api endpoint that
@@ -17,25 +15,32 @@ let defaultteam = document.getElementById("defaultteam");
 let url =
     "https://cs684api.7g2rji63c728k.us-east-1.cs.amazonlightsail.com/hw3?team=";
 
+//flag added to isolate the input box data from the default search team
+let startflag = 0;
+
+//set global team name
+let team_name = "";
+
 //function that makes an ajax call to a remote
 //api and updates the teamname and token values
 //in the html document with the requested values
 let getTeamInformation = () => {
     //on window load set team name to default. Changing teams
     //updates value
-    if (changeteam.style.display == "none") {
-        team_name = teamheader.value;
-    } else {
+    if(startflag == 0){
         team_name = getTeam();
     }
+    else{
+        team_name = teamheader.value;
+    }
 
-    console.log(team_name);
     $.ajax({
         url: url + team_name,
         type: "GET",
         crossDomain: true,
         dataType: "json",
         success: function (data) {
+            console.log(data)
             teamheader.value = data.team;
             tokenvalue.value = data.token;
         },
@@ -47,31 +52,20 @@ let getTeamInformation = () => {
 
 //Get Default team name and on page load
 let getTeam = () => {
-    return "Team Z";
+    return "Z";
 };
 
-let searchCustom = event => {
-    if (changeteam.style.display != "none") {
-        console.log("HERE UP serchcustom");
-        teamheader.value = "";
-        tokenvalue.value = "";
-        defaultteam.style.display = "inline-block";
-        changeteam.style.display = "none";
-    } else {
-        console.log("HERE down serchcustom");
-        changeteam.style.display = "inline-block";
-        defaultteam.style.display = "none";
-        teamheader.value = getTeam();
-        tokenvalue.innerHTML = "";
-        getTeamInformation();
-    }
-};
-
-//call function on window load
-window.onload = () => {
+//Update the teamname input box to the default value and query the api
+let setDefault = (event) => {
+    teamheader.value = getTeam();
     getTeamInformation();
 };
 
-changeteam.addEventListener("click", searchCustom);
-defaultteam.addEventListener("click", searchCustom);
+//call api on window load
+window.onload = () => {
+    getTeamInformation();
+    startflag = 1;
+};
+
+defaultteam.addEventListener("click", setDefault);
 apicall.addEventListener("click", getTeamInformation);
